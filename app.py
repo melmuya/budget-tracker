5c615f1f19233b9a78ddce7e332d893a60324230
Mel <melchizedek.maranga@gmail.com>
2025-05-04 20:40:07 +0300

    increased password hash column length

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     
     # Relationship to other models
